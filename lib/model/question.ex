2e3a39e99678d882250a1710aadcb3edeb450d11
Megumi Ishikawa <megumi.ishikawa@access-company.com>
2020-05-08 15:40:11 +0900

    Question APIの実装 (#1)

--- lib/model/question.ex
+++ lib/model/question.ex
@@ -1,6 +1,36 @@
 use Croma
-
 defmodule StackoverflowCloneA.Model.Question do
-  # Implement me
+  defmodule Title do
+    use Croma.SubtypeOfString, pattern: ~R/\A.{1,100}\z/u
+  end
+  defmodule Body do
+    use Croma.SubtypeOfString, pattern: ~R/\A.{1,3000}\z/u
+  end
+  defmodule VoterIdList do
+    use Croma.SubtypeOfList, elem_module: StackoverflowCloneA.DodaiId
+  end
+  defmodule Comment do
+    use Croma.Struct, recursive_new?: true, fields: [
+      id: StackoverflowCloneA.DodaiId,
+      body: Body,
+      user_id: StackoverflowCloneA.DodaiId,
+      created_at: Croma.String,
+    ]
+  end
+  defmodule CommentList do
+    use Croma.SubtypeOfList, elem_module: Comment
+  end
+  defmodule StackoverflowCloneA.DodaiId do
+    # See [RFC3986](https://tools.ietf.org/html/rfc3986#section-2.3) for the specifications of URL-safe characters
+    @url_safe_chars "0-9A-Za-z\-._~"
+    use Croma.SubtypeOfString, pattern: ~r"\A[#{@url_safe_chars}]+\Z"
+  end
+  use AntikytheraAcs.Dodai.Model.Datastore, data_fields: [
+    title:             Title,
+    body:              Body,
+    user_id:           StackoverflowCloneA.DodaiId,
+    like_voter_ids:    VoterIdList,
+    dislike_voter_ids: VoterIdList,
+    comments:          CommentList,
+  ]
 end
-
--- lib/repo/question.ex
+++ lib/repo/question.ex
@@ -1,9 +1,7 @@
 use Croma
 
 defmodule StackoverflowCloneA.Repo.Question do
-  # 下記の関数はダミーの関数です。
-  # 下記を先に削除してください。
-  def retrieve_list(_, _) do
-    {:ok, []}
-  end
+  use AntikytheraAcs.Dodai.Repo.Datastore, [
+    datastore_models: [StackoverflowCloneA.Model.Question],
+  ]
 end
