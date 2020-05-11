90ad4b8972ff6c2bf13a6e45424537bf8f5349ff
Ikebe Soichi <souichi1997@gmail.com>
2020-05-11 16:48:31 +0900

    add update question api (#5)

--- test/web/controller/question/update_test.exs
+++ test/web/controller/question/update_test.exs
@@ -1,7 +1,51 @@
 defmodule StackoverflowCloneA.Controller.Question.UpdateTest do
   use StackoverflowCloneA.CommonCase
+  alias StackoverflowCloneA.Repo.Question, as: RQ
+  alias StackoverflowCloneA.TestData.QuestionData
 
-  test "update/1 " do
-    # Implement me
+  @question   QuestionData.model()
+  @api_prefix "/v1/question/#{@question._id}"
+  @body       %{"title" => "title"}
+  @header     %{"authorization" => "hYniRi3lFlsFVMmERimC"}
+
+  describe "update/1 " do
+    test "should update question" do
+      # userを取得する処理をmock
+      user = StackoverflowCloneA.TestData.UserData.model()
+      mock_fetch_me_plug(user) 
+    
+      # Questionを取得する処理をmock
+      :meck.expect(RQ, :retrieve, fn(id, _key) ->
+        assert id == @question._id
+        {:ok, @question}
+      end)
+    
+      # Questionを更新する処理をmock
+      :meck.expect(RQ, :update, fn(data, id, _key) ->
+        assert id == @question._id
+        assert data == %{data: %{"$set" => %{title: "title"}}}
+        {:ok, @question}
+      end)
+    
+      res = Req.put_json(@api_prefix, @body, @header)
+      assert res.status               == 200
+      assert Jason.decode!(res.body) == QuestionData.gear()
+    end
+
+    test "should return ResourceNotFoundError when specified question is not found" do
+      :meck.expect(RQ, :retrieve, fn(_id, _key) ->
+        {:error, %Dodai.ResourceNotFound{}}
+      end)
+      :meck.expect(RQ, :update, fn(_data, _id, _key) -> flunk() end)
+
+      res = Req.put_json(@api_prefix, @body, @header)
+      assert res.status              == 404
+      assert Jason.decode!(res.body) == %{
+        "code"        => "404-04",
+        "description" => "The resource does not exist in the database.",
+        "error"       => "ResourceNotFound",
+      }
+    end
+
   end
 end
--- web/controller/question/update.ex
+++ web/controller/question/update.ex
@@ -1,7 +1,62 @@
+use Croma
+
 defmodule StackoverflowCloneA.Controller.Question.Update do
   use StackoverflowCloneA.Controller.Application
+  alias StackoverflowCloneA.Model.User
+  alias StackoverflowCloneA.Repo.Question, as: RQ
+  alias StackoverflowCloneA.Error.{BadRequest, ResourceNotFound, InvalidCredential}
+  alias StackoverflowCloneA.Controller.Question.Helper
 
-  def update(_conn) do
-    # Implement me
+  plug StackoverflowCloneA.Plug.FetchMe, :fetch, []
+
+  # パラメータvalidation用のmodule
+  defmodule RequestBody do
+    alias StackoverflowCloneA.Model.Question
+    use Croma.Struct, fields: [
+      title:              Croma.TypeGen.nilable(Question.Title),
+      body:               Croma.TypeGen.nilable(Question.Body),
+    ]
   end
+
+  defun update(%Conn{request: %Request{path_matches: %{id: id}, body: body}, assigns: %{me: %User{_id: user_id}}} = conn) :: Conn.t do
+    # 第二引数で指定したidのquestionを取得するための関数です。
+    with_question(conn, id, fn question ->
+      # 更新対象のquestionのuser_idとログインユーザの_idが一致するか確認する(この判定ロジックを書いてみましょう)
+      ## 一致しない場合、下記のようにエラーを返す
+      if question.data.user_id != user_id do
+        ErrorJson.json_by_error(conn, InvalidCredential.new())
+      else
+        case RequestBody.new(body) do
+          {:error, _}      ->
+            ErrorJson.json_by_error(conn, BadRequest.new())
+          {:ok, validated} ->
+            data = to_update_document(validated)
+            res = RQ.update(%{data: data}, id, StackoverflowCloneA.Dodai.root_key())
+            case res do
+              {:ok, question}                     -> Conn.json(conn, 200, Helper.to_response_body(question))
+              {:error, %Dodai.ResourceNotFound{}} -> ErrorJson.json_by_error(conn, ResourceNotFound.new())
+            end
+        end
+      end
+    end)
+  end
+
+  defun with_question(conn :: v[Conn.t], id :: v[DodaiId.t], f :: ((map) -> Conn.t)) :: Conn.t do
+    res = RQ.retrieve(id, StackoverflowCloneA.Dodai.root_key())
+    case res do
+      # Questionの取得に成功した場合は、第3引数で指定された関数を実行します。
+      {:ok, question}  -> f.(question)
+      # Questionの取得に失敗した場合は、エラーを返します。
+      {:error, _}      -> ErrorJson.json_by_error(conn, StackoverflowCloneA.Error.ResourceNotFound.new())
+    end
+  end
+
+  defunp to_update_document(body :: v[RequestBody.t]) :: map do
+    data =
+      Map.from_struct(body)
+      |> Enum.reject(fn {_, value} -> is_nil(value) end)
+      |> Enum.into(%{})
+    %{"$set" => data}
+  end
+
 end
