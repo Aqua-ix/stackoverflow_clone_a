44aa8d2971a3a6e79d10b6bc0271c0b53ffca7d5
Ikebe Soichi <souichi1997@gmail.com>
2020-05-13 13:27:40 +0900

    add Question API index (#11)

--- web/controller/question/index.ex
+++ web/controller/question/index.ex
@@ -4,10 +4,35 @@ defmodule StackoverflowCloneA.Controller.Question.Index do
   use StackoverflowCloneA.Controller.Application
   alias StackoverflowCloneA.Controller.Question.Helper
   alias StackoverflowCloneA.Repo.Question, as: RQ
+  alias StackoverflowCloneA.Error.BadRequest
+  
+  defmodule QueryParams do
+    use Croma.Struct, fields: [
+      user_id:  Croma.TypeGen.nilable(StackoverflowCloneA.DodaiId),
+    ]
+  end
 
-  defun index(conn :: v[Conn.t]) :: Conn.t do
-    query = %{sort: %{"_id" => 1}}
-    {:ok, questions} = RQ.retrieve_list(query, StackoverflowCloneA.Dodai.root_key())
-    Conn.json(conn, 200, Enum.map(questions, &Helper.to_response_body/1))
+  defun index(%Conn{request: %Request{query_params: query_params}} = conn :: v[Conn.t]) :: Conn.t do
+    case QueryParams.new(query_params) do
+      {:error, _}      ->
+        ErrorJson.json_by_error(conn, BadRequest.new())
+      {:ok, validated} ->
+        query = convert_to_dodai_req_query(validated)
+        IO.inspect(query)
+        {:ok, question} = RQ.retrieve_list(query, StackoverflowCloneA.Dodai.root_key())
+        Conn.json(conn, 200, Enum.map(question, &Helper.to_response_body(&1)))
+    end
+  end
+
+  defunpt convert_to_dodai_req_query(params :: v[QueryParams.t]) :: map do
+    query =
+      Map.from_struct(params)
+      |> Enum.reject(fn {_, value} -> is_nil(value) end)
+      |> Enum.map(fn {k, v} -> {"data.#{k}", v} end)
+      |> Map.new()
+    %{
+      query: query,
+      sort:  %{"_id" => 1}
+    }
   end
 end
